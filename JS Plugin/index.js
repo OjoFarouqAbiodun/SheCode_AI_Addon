5215ea933d3295e85dbfe1c1534208f6436016ee
Farouq <harbyodunojo@gmail.com>
2026-07-08 18:32:24 +0100

    Practiced using JS plugin with typewriter JS plugin

---
+++ JS Plugin/index.js
@@ -0,0 +1,9 @@
+let challenge2Text = document.querySelector("#challenge-2");
+challenge2Text.innerHTML = "Harbey Graphics";
+
+new Typewriter("#challenge-3", {
+	strings: "Harbey Graphics",
+	autoStart: true,
+	cursor: "",
+	delay: 75,
+});
